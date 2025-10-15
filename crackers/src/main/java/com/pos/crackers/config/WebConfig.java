6d0b26b0355d3403042aacc7953ac4efcf939af7
dilip <pddd19@gmail.com>
2025-10-15 23:26:50 +0530

    added CORS error fix

--- crackers/src/main/java/com/pos/crackers/config/WebConfig.java
+++ crackers/src/main/java/com/pos/crackers/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:5173","https://purchase.balajicrackers.shop","https://balajicrackers.shop","http://my-crackers-bucket.s3-website-us-east-1.amazonaws.com","http://localhost:3000") // Change this to your frontend's origin
+                .allowedOrigins("http://localhost:5173","balajicrackers.store","https://balajicrackers.store","http://my-crackers-bucket.s3-website-us-east-1.amazonaws.com","http://localhost:3000") // Change this to your frontend's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept") // Allow necessary headers
                 .allowCredentials(true); // Allow credentials like cookies
